2d9cdea6925bf6387ee89992b3f0b7c561e11589
VECTOR-BEAN <zijiebing@163.com>
2020-05-24 20:41:44 +0800

    update 1004 BFS

--- 1004 Counting Leaves BFS.cpp
+++ 1004 Counting Leaves BFS.cpp
@@ -1,35 +1,28 @@
-#include <iostream>
 #include <cstdio>
 #include <vector>
 #include <queue>
-
 using namespace std;
 const int MAXN = 100;
 
 struct Node {
     int layer;
     vector<int> son;
-};
+} tree[MAXN];
 
-Node tree[MAXN];
-int leaf[MAXN] = {0};
-int nowLayer = 1;
+int maxLayer = 1, leaf[MAXN] = {0};
 
 void BFS() {
     queue<Node> q;
+    tree[1].layer = 1;
     q.push(tree[1]);
     while (!q.empty()) {
-        Node n = q.front(), son;
+        Node node = q.front();
         q.pop();
-        vector<int> sonVec = n.son;
-		if (n.layer != nowLayer) nowLayer++;
-        if (sonVec.size() == 0) {  // if it is a leaf
-            leaf[nowLayer]++;
-        }
-        for (int i = 0; i < sonVec.size(); i++) {
-            son = tree[sonVec[i]];
-            son.layer = n.layer + 1;
-            q.push(son);
+		if (node.layer > maxLayer) maxLayer = node.layer;
+        if (node.son.empty()) leaf[node.layer]++;
+        for(auto sonId: node.son){
+        	tree[sonId].layer = node.layer + 1;
+        	q.push(tree[sonId]);
         }
     }
 }
@@ -37,23 +30,20 @@ void BFS() {
 int main() {
     int N, M;
     scanf("%d%d", &N, &M);
-    // case with N being 0 mustn't be processed
-    if (N == 0) return 0;  
-    // else
-    tree[1].layer = 1;
-    for (int i = 0, id, k; i < M; i++) {
-        scanf("%d %d", &id, &k);
-        for (int j = 0, temp; j < k; j++) {
-            scanf("%d", &temp);
-            tree[id].son.push_back(temp);
+    for (int i = 0, id, num; i < M; i++) {
+        scanf("%d%d", &id, &num);
+        for (int j = 0, sonId; j < num; j++) {
+            scanf("%d", &sonId);
+            tree[id].son.push_back(sonId);
         }
     }
 
     BFS();
 
-    for (int i = 1; i < nowLayer; i++) {
-        printf("%d ", leaf[i]);
+    for (int i = 1; i <= maxLayer; i++) {
+        printf("%d", leaf[i]);
+        if(i < maxLayer) printf(" ");
+        else printf("\n");
     }
-    printf("%d", leaf[nowLayer]);
     return 0;
 }
