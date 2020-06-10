2399d11ad3ca9a258876e839229b52db6cf6ed8c
VECTOR-BEAN <zijiebing@163.com>
2020-06-10 17:24:30 +0800

    update 1023 1024

--- 1024 Palindromic Number.cpp
+++ 1024 Palindromic Number.cpp
@@ -2,36 +2,20 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-
-bool palindromic(vector<int> num) {
-	int len = num.size();
-	for(int i = 0; i < len/2; i++) {
-		if(num[i] != num[len-1-i]) return false;
-	}
-	return true;
-}
-
 int main() {
-	int maxStep, curStep = 0, carry = 0;
+	int maxStep, carry, curStep;
 	vector<int> num, tempNum;
-	string rawNum;
-	cin >> rawNum >> maxStep;
-	for(int i = rawNum.size() - 1; i >= 0; i--) {
-		num.push_back(rawNum[i] - '0');
-	}
-	while(!palindromic(num) && curStep < maxStep) {
-		curStep++;
-		tempNum = num;
+	for(char temp = getchar(); temp != ' '; temp = getchar()) num.push_back(temp - '0');
+	for(scanf("%d", &maxStep), curStep = 0; curStep < maxStep; curStep++) {
+		tempNum = num; carry = 0;
 		reverse(tempNum.begin(), tempNum.end());
-		for(int i = 0, len = num.size(), temp; i < len; i++) {
+		if(tempNum == num) break;
+		for(int i = 0, temp; i < num.size(); i++) {
 			temp = num[i] + tempNum[i] + carry;
 			num[i] = temp % 10;
 			carry = temp / 10;
 		}
-		if(carry != 0) {
-			num.push_back(carry);
-			carry = 0;
-		}
+		if(carry) num.push_back(carry);
 	}
 	for(int i = num.size() - 1; i >= 0; i--) printf("%d", num[i]);
 	printf("\n%d\n", curStep);
