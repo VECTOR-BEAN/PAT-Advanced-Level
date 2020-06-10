f18619714691a6f8339bbc8a064877fb299a1981
VECTOR-BEAN <zijiebing@163.com>
2020-06-10 17:24:30 +0800

    update 1023 1024

--- 1023 Have Fun with Numbers.cpp
+++ 1023 Have Fun with Numbers.cpp
@@ -1,35 +1,21 @@
 #include<iostream>
-#include<cstring>
 #include<memory.h>
 using namespace std;
-
 int main() {
-	int len, size = 0, carry=0, flag = 1,res[21], cnt[10];
-	char bigNum[20];
-	memset(cnt, 0, sizeof(cnt));
-	scanf("%s", bigNum);
-	len = strlen(bigNum);
-	for(int i = len - 1; i >= 0; i--, size++) {
-		int digit = bigNum[i] - '0';
-		int temp = 2 * digit + carry;
-		carry = temp / 10;
-		res[size] = temp % 10;
-		cnt[digit]++;
-		cnt[res[size]]--;
+	int sz = 0, flag = 1, carry = 0, num[21], hashTable[10];
+	memset(hashTable, 0, sizeof(hashTable));
+	for(char tempNum = getchar(); tempNum != '\n'; tempNum = getchar()) num[sz++] = tempNum - '0';
+	for(int i = sz - 1, d; i >= 0; i--) {
+		d = num[i];
+		hashTable[num[i]]++;
+		num[i] = (d * 2 + carry) % 10;
+		carry = (d * 2 + carry) / 10;
+		hashTable[num[i]]--;
 	}
-	if(carry != 0) res[size++] = carry;
-
-	for(auto i:cnt) {
-		if(i != 0 || size > len) {
-			flag = 0;
-			break;
-		}
-	}
-	if(flag) printf("Yes\n");
-	else printf("No\n");
-	for(int i = size-1; i >= 0; i--) {
-		printf("%d", res[i]);
-	}
+	for(auto i:hashTable) if(i != 0 || carry != 0) flag = 0;
+	printf("%s\n", flag == 1?"Yes":"No");
+	if(carry) printf("%d", carry);
+	for(int i = 0; i < sz; i++) printf("%d", num[i]);
 	printf("\n");
 	return 0;
 }
